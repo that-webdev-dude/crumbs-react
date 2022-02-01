35337ecd32f6c224ecc2ad0c84aa21ff37ba0cdb
that-webdev-dude <that.webdev.dude@gmail.com>
2022-02-01 20:49:09 +0000

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// pages //
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Recipe from './pages/Recipe';
+import Search from './pages/Search';
+
 // styles //
 import './App.css';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/create'>
+            <Create></Create>
+          </Route>
+          <Route path='/recipe'>
+            <Recipe></Recipe>
+          </Route>
+          <Route path='/search'>
+            <Search></Search>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
