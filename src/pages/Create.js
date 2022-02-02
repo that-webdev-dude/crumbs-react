adc5831c6a64a5bbee3cd68b337fecedc1449c60
that-webdev-dude <that.webdev.dude@gmail.com>
2022-02-02 21:05:44 +0000

    pages

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,21 @@
+// components //
+import AppContainer from '../components/AppContainer';
+
 // styles //
-import './Create.css';
+import './Create.scss';
 
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <div className='create'>
+      <AppContainer>
+        <h2 className='page-title'>Create</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+          modi rerum quae, voluptatum doloremque enim? Nemo consequuntur,
+          provident nobis ipsam eum maiores possimus ea, mollitia laboriosam vel
+          molestiae, cumque aliquam.
+        </p>
+      </AppContainer>
+    </div>
+  );
 }
